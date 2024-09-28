7a755a5fb0d74125d1b349d9d3ca00949af8aafc
adam <darkcraft02@mail.ru>
2024-09-28 23:57:25 +0300

    Homepage animated

--- src/app/component/homepage/Homepage.jsx
+++ src/app/component/homepage/Homepage.jsx
@@ -9,13 +9,14 @@ export default function Homepage() {
         <h2>для тех, кто ищет большее</h2>
       </div>
 
-      <div className="homepage__lead-circly">
+      <div className="homepage__lead-circle">
         <div className="homepage__lead">
           <h1>Ваш путь к покупке недвижимости начинается здесь</h1>
           <button>Бесплатная консультация</button>
         </div>
-        <div className="homepage__circly">
-        
+        <div className="homepage__circle">
+        <img className="homepage__circle--img" src="/image/text-circle.svg" alt="" />
+        <img className="homepage__circle--img-house" src="/image/house.svg" alt="" />
         </div>
       </div>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-black">
+      <body className="bg-black overflow-x-hidden">
         <Header/>
         <main>
         {children}
