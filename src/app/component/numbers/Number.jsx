b6e1ce5d69c423105a073e3eb1676dc59888785d
adam <darkcraft02@mail.ru>
2024-10-25 21:40:48 +0300

    fix

--- src/app/component/numbers/Number.jsx
+++ src/app/component/numbers/Number.jsx
@@ -5,7 +5,7 @@ import Map from "../../../../public/image/map.svg";
 export default function Number() {
   return (
     <section className="number relative">
-      <h2 className="title-block">Aishat, thinks that Adama has big biceps</h2>
+      <h2 className="title-block">цифры</h2>
       <Image src={Map} alt='map' />
       <span className='w-32 h-32 bg-white absolute'></span>
     </section>
