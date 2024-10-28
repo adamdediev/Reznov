aa0c12897ca3c20663c534555b5cd954fb12c247
adam <darkcraft02@mail.ru>
2024-10-28 22:35:01 +0300

    Added contactform

--- src/app/component/contactForm/ContactForm.jsx
+++ src/app/component/contactForm/ContactForm.jsx
@@ -1,9 +1,21 @@
+import {Button} from "@nextui-org/button";
 
 export default function ContactForm() {
   return (
-    <section>
+    <section className="contact-form">
         <h2 className="title-block">свяжитесь с нами</h2>
+        <div className="contact-form__block">
+          <div className="contact-form__title w-1/2">
+          <span>доверьте сделку с недвижимостью</span>
+          профессионалам
+          </div>
+          <div className="contact-form__formgroup w-1/2">
 
+          <Button  className="contact-form__formgroup-button">
+          свяжитесь со мной
+          </Button>
+          </div>
+        </div>
 
     </section>
   )
