6646a6335bf766ff29b732a1aa305f093c2fd347
adam <darkcraft02@mail.ru>
2025-09-11 00:53:50 +0300

    Fix text ;

--- src/app/component/secondary-real-estate-search/homepage/Homepage.jsx
+++ src/app/component/secondary-real-estate-search/homepage/Homepage.jsx
@@ -16,17 +16,22 @@ export default function Homepage() {
         <div className="container homepage-secondary">
         <div className="homepage-secondary__content">
           <h1><strong>Профессионально подбор недвижимости</strong></h1>
-          <div> <strong>
+          <div> 
             
 
-<strong className='font-semibold'>Для Вас:</strong> 
-Подберем лучший вариант всего за 14 дней
-Получим скидку на квартиру до 30%
-Поможем получить одобрение ипотеки со ставкой от 6%
-Проверим юридическую чистоту объекта по 12 параметрам
-Проведем сделку под ключ <strong className='font-semibold'> на Ваших условиях</strong>  
+  <strong className='font-semibold'>Для Вас:</strong>
+  <ul>
+    <strong>
+    <li>- Подберем лучший вариант всего за 14 дней</li>
+    <li>- Получим скидку на квартиру до 30%</li>
+    <li>- Поможем получить одобрение ипотеки со ставкой от 6%</li>
+    <li>- Проверим юридическую чистоту объекта по 12 параметрам</li>
+    <li>- Проведем сделку под ключ на Ваших условиях</li>
+    </strong>
+  </ul>
+
             
-            </strong>
+            
             </div>
           <button onClick={handleOpen}>БЕСПЛАТНАЯ КОНСУЛЬТАЦИЯ</button>
         </div>
